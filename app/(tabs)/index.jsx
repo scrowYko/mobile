6759be962c588d453532dd3f9175627b19cbaa9e
DAVI HENRY MOREL PINTOS <davi_pintos@estudante.sesisenai.org.br>
2024-08-15 12:22:37 -0300

    aaa

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, View, TextInput, Pressable, FlatList } from "react-native";
-import { novaTarefa } from "../../scripts/listaTarefas";
+import { novaTarefa, alterBoolean } from "../../scripts/listaTarefas";
 
 const style = function () {
   return {
@@ -9,12 +9,49 @@ const style = function () {
       justifyContent: "center",
       alignItems: "center",
     },
+    container2: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "row",
+      gap: 20,
+    },
     isDone: {
-      textDecoration: "lineThrough",
+      textDecorationLine: "line-through",
+      backgroundColor: '#F1F3F4', // Cinza um pouco mais escuro
+      color: 'rgba(0, 0, 0, 0.5)', // Letra com opacidade reduzida
+      width: 160,
+      flex: 1,
+      textAlign: 'center',
+      justifyContent: 'center',
+      height: 60,
+      borderRadius: 8,
+      border: 'solid #0077b6 3px',
     },
     notDone: {
-      backgroundColor: "red",
+      backgroundColor: '#FFFFFF', // Cinza bem claro
+      textDecorationLine: 'none',
+      color: 'rgba(0, 0, 0, 1)', // Letra com opacidade normal
+      width: 160,
+      flex: 1,
+      textAlign: 'center',
+      justifyContent: 'center',
+      height: 60,
+      borderRadius: 8,
+      border: 'solid #35E0FF 3px',
     },
+    input:{
+      width:200,
+      flex: 1,
+      backgroundColor: '#F1F3F4',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 40,
+      height: 60,
+      borderRadius: 4,
+      border: 'solid #000000 3px',
+      padding: 6
+    }
   };
 };
 
@@ -28,24 +65,29 @@ export default function listaTarefas() {
 
   return (
     <View style={style().container}>
-      <View>
+      <View style={style().container2}>
         <TextInput
           onChangeText={onChangeText}
           placeholder="Digite aqui"
           value={text}
+          style={style().input}
         />
-        <Pressable onPress={() => novaTarefa(text, tarefas, atualizarTarefas)}>
+        <Pressable onPress={() => novaTarefa(text, tarefas, atualizarTarefas)} style={style().input}>
           <Text>Adicionar</Text>
         </Pressable>
       </View>
       <FlatList
         data={tarefas}
         renderItem={({ item }) => {
-          console.log(item)
-           return item.isDone == false ? (
-            <Text style={style().notDone}>{item.title}</Text>
-          ) : (
-            <Text style={style().isDone}>{item.title}</Text>
+          console.log(item);
+          return (
+            <Text >
+              <Pressable style={item.isDone == true ? style().isDone : style().notDone}
+                onPress={() => alterBoolean(item.id, tarefas, atualizarTarefas)}
+              >
+                {item.title}
+              </Pressable>
+            </Text>
           );
         }}
         keyExtractor={(item) => item.id.toString()}
--- scripts/listaTarefas.js
+++ scripts/listaTarefas.js
@@ -1,10 +1,11 @@
 let taskID = 0
 
 class Tarefa{
-    constructor(title, isDone = false){
+    constructor(title, isDone = false, date){
         this.id = ++taskID
         this.title = title
         this.isDone = isDone
+        this.date = date
     }
 }
 
@@ -14,3 +15,14 @@ export const novaTarefa = (title, array, atualizarTarefas) => {
    return true
 }
 
+export const alterBoolean = (id, array, atualizarTarefas) => {
+    let updatedArray = array.map((tarefa) => {
+        if (tarefa.id === id) {
+            return { ...tarefa, isDone: !tarefa.isDone };
+        }
+        return tarefa;
+    });
+    atualizarTarefas(updatedArray);
+    console.log(updatedArray.find(tarefa => tarefa.id === id).isDone);
+    return true;
+}
