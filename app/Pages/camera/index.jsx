3af03adb8fe05063a5ae3c33324bd286e42008ef
DAVI HENRY MOREL PINTOS <davi_pintos@estudante.sesisenai.org.br>
2024-10-03 12:37:34 -0300

    salvando camera

--- app/Pages/camera/index.jsx
+++ app/Pages/camera/index.jsx
@@ -1,101 +1,91 @@
 import { React, useState, useEffect, useRef } from "react";
-import { View, Text, Image, StyleSheet, Pressable, } from 'react-native';
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import { CameraView, useCameraPermissions } from "expo-camera";
-import { Button } from "react-native-web";
-
-
-
 
 export default function Camera() {
-    const [foto, setFoto] = useState(null);
-    const [permissao, pedirPermissao] = useCameraPermissions()
-    const cameraRef = useRef(null)
-    const [lado, setLado] = useState('back')
+  const [foto, setFoto] = useState(null);
+  const [permissao, pedirPermissao] = useCameraPermissions();
+  const cameraRef = useRef(null);
+  const [lado, setLado] = useState("back");
 
-    const tirarFoto = async () => {
-        const fotoBase64 = await cameraRef.current?.takePictureAsync({
-            quality: 0.5,
-            base64: true
-        })
-        setFoto(fotoBase64)
-        console.log(foto)
-    }
+  const tirarFoto = async () => {
+    const fotoBase64 = await cameraRef.current?.takePictureAsync({
+      quality: 0.5,
+      base64: true,
+    });
+    setFoto(fotoBase64);
+    console.log(foto);
+  };
 
-    const trocaCamera = () => {
-        setLado(lado == 'back' ? 'front' : 'back')
-    }
+  const trocaCamera = () => {
+    setLado(lado == "back" ? "front" : "back");
+  };
 
-    if (!permissao) {
-        return <View></View>
-    }
-    if (!permissao.granted) {
-
-
-
-        return (
-            <View style={style.container}>
-                <Text style={style.textoPermissão}>Você precisa conceder a permissão para usar a câmera</Text>
-                <Pressable onPress={pedirPermissao} style={style.button}>
-                    <Text style={style.buttonText}>Pedir permissão</Text>
-                </Pressable>
-            </View>
-        )
-    }
-
+  if (!permissao) {
+    return <View></View>;
+  }
+  if (!permissao.granted) {
     return (
-        <View>
-            {foto ?
-                <View>
-                    <Image source={{ uri: pic.uri }} style={style.foto} />
-                    <Pressable onPress={setFoto(null)} style={style.button}><Text style={style.buttonText}>Limpar foto</Text></Pressable>
-                </View> :
-                <CameraView facing={{lado}} style={style.camera} ref={cameraRef}>
-                    <Pressable onPress={tirarFoto}>
-                        <Text>Tirar foto</Text>
-                    </Pressable>
-                    <Pressable onPress={() => trocaCamera}>
-                        <Text>Inverter Câmera</Text>
-                    </Pressable>
-                </CameraView>
-            } </View>
+      <View style={style.container}>
+        <Text style={style.textoPermissão}>
+          Você precisa conceder a permissão para usar a câmera
+        </Text>
+        <Pressable onPress={pedirPermissao} style={style.button}>
+          <Text style={style.buttonText}>Pedir permissão</Text>
+        </Pressable>
+      </View>
     );
+  }
+
+  return (
+    <View style={style.container}>
+      {foto ? (
+        <View>
+          <Image source={{ uri: pic.uri }} style={style.foto} />
+          <Pressable onPress={setFoto(null)} style={style.button}>
+            <Text style={style.buttonText}>Limpar foto</Text>
+          </Pressable>
+        </View>
+      ) : (
+        <CameraView facing={lado} style={style.camera} ref={cameraRef}>
+          <Pressable onPress={tirarFoto}>
+            <Text>Tirar foto</Text>
+          </Pressable>
+          <Pressable onPress={() => trocaCamera}>
+            <Text>Inverter Câmera</Text>
+          </Pressable>
+        </CameraView>
+      )}
+    </View>
+  );
 }
 
 const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginLeft: '25%',
-        marginRight: '25%',
-        marginTop: '25%',
-        marginBottom: '25%',
-        borderColor: '#d1d1d1',
-        borderRadius: 15,
-        borderWidth: 1,
-        padding: 20,
-    },
-    textoPermissão: {
-        fontSize: 21,
-    },
-    button: {
-        backgroundColor: '#69BA5D',
-        borderRadius: 10,
-        width: 120,
-        height: 40,
-        padding: 5
-    },
-    buttonText: {
-        fontSize: 14,
-    },
-    camera: {
-        flex: 1
-    },
-    tirarFoto: {
-        backgroundColor: 'white'
-    },
-    foto: {
-        width: "100%",
-        height: '100%'
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  textoPermissão: {
+    fontSize: 21,
+  },
+  button: {
+    backgroundColor: "#69BA5D",
+    borderRadius: 10,
+    width: 120,
+    height: 40,
+    padding: 5,
+  },
+  buttonText: {
+    fontSize: 14,
+  },
+  camera: {
+    flex: 1,
+  },
+  tirarFoto: {
+    backgroundColor: "white",
+  },
+  foto: {
+    width: "100%",
+    height: "100%",
+  },
+});
